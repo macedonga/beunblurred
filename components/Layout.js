948e782fd98b5e59bdb314ee2b7a558465224665
Marco Ceccon <me@marco.win>
2023-07-17 14:28:42 +0200

    Better mobile layout

--- components/Layout.js
+++ components/Layout.js
@@ -6,8 +6,8 @@ export default function Layout({ children }) {
     return (<>
         <div
             className={`
-                min-h-screen flex flex-col
-                py-8 max-w-xl mx-auto ${inter.className}
+                flex flex-col h-full overflow-auto w-full
+                pt-8 max-w-xl mx-auto ${inter.className}
             `}
         >
             <header className="pb-8 border-b-2 border-white/10">
@@ -16,28 +16,31 @@ export default function Layout({ children }) {
                 </h1>
             </header>
 
-            <main className="lg:p-8 p-4">{children}</main>
-
-            <footer
-                className={`
-                    pt-8 lg:px-8 px-4 text-sm text-center
-                    border-t-2 border-white/10 mt-auto
-                `}
-            >
-                <p>
-                    <b>This site is in no way affiliated with BeReal SAS.</b>
-                    <br />
-                    This is a school project made by{" "}
-                    <a
-                        href="https://marco.win"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="underline decoration-dashed hover:opacity-75 transition-all"
-                    >
-                        Marco Ceccon
-                    </a>.
-                </p>
-            </footer>
+            <main className="lg:p-8 p-4 pb-0">
+                {children}
+            </main>
         </div>
+
+        <footer
+            className={`
+                    sticky bottom-0 max-w-xl mx-auto w-full
+                    py-8 lg:px-8 px-4 text-sm text-center
+                    border-t-2 border-white/10 mt-auto ${inter.className}
+                `}
+        >
+            <p>
+                <b>This site is in no way affiliated with BeReal SAS.</b>
+                <br />
+                This is a school project made by{" "}
+                <a
+                    href="https://marco.win"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="underline decoration-dashed hover:opacity-75 transition-all"
+                >
+                    Marco Ceccon
+                </a>.
+            </p>
+        </footer>
     </>);
 }

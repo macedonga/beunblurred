55ec2c2294f541338cd35fce6cdd6616d34a6374
Marco Ceccon <me@marco.win>
2023-08-26 18:06:21 +0200

    Better desktop layout

--- components/Layout.js
+++ components/Layout.js
@@ -12,7 +12,7 @@ export default function Layout({ children }) {
             `}
         >
             <Link href="/feed">
-                <header className="py-8 border-b-2 px-4 lg:border-x-2 border-white/10 bg-[#0d0d0d]">
+                <header className="py-8 border-b-2 px-4 lg:border-x-2 lg:rounded-b-lg border-white/10 bg-[#0d0d0d]">
                     <h1 className="text-4xl font-bold text-center">
                         BeUnblurred.
                     </h1>
@@ -22,14 +22,14 @@ export default function Layout({ children }) {
                 </header>
             </Link>
             
-            <main className="lg:px-8 px-4 py-8">
+            <main className="lg:px-0 px-4 py-8">
                 {children}
             </main>
 
             <footer
                 className={`
                     mt-auto lg:max-w-xl mx-auto w-full bg-[#0d0d0d]
-                    py-8 lg:px-8 px-4 text-sm text-center
+                    py-8 lg:px-8 px-4 text-sm text-center lg:rounded-t-lg
                     border-t-2 lg:border-x-2 border-white/10 ${inter.className}
                 `}
             >

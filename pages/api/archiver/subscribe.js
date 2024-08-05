32345cb14150ad53dbf12e4cac425f4217b5ecbb
Marco Ceccon <me@marco.win>
2024-08-05 15:57:40 +0200

    Fix url

--- pages/api/archiver/subscribe.js
+++ pages/api/archiver/subscribe.js
@@ -35,8 +35,8 @@ export default async function handler(req, res) {
             },
         ],
         mode: "subscription",
-        success_url: "http://localhost:3000/archiver/load",
-        cancel_url: "http://localhost:3000/archiver",
+        success_url: "https://www.beunblurred.co/archiver/load",
+        cancel_url: "https://www.beunblurred.co/archiver",
     });
 
     res.status(200).redirect(session.url);
